15422842af8a7bdff9f947e2f090f3726d197958
vaishb123 <83326651+vaishb123@users.noreply.github.com>
2024-05-22 09:34:54 +0530

    Update appi.py

--- appi.py
+++ appi.py
@@ -4,3 +4,13 @@ st.text("Streamlit App my_first_app")
 st.header("App header")
 
 st.header("My First App")
+
+st.text("Streamlit App my_first_app")
+st.header("App header")
+
+st.header("My First App")
+
+st.text("Streamlit App my_first_app")
+st.header("App header")
+
+st.header("My First App")
