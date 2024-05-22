7be0f11c6bc5f5b65d364ec1dd4c04f92aade510
vaishb123 <83326651+vaishb123@users.noreply.github.com>
2024-05-22 11:40:00 +0530

    Update appi.py

--- appi.py
+++ appi.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.text("Streamlit App my_first_app")
 st.header("App header")
@@ -15,3 +16,5 @@ st.header("App header")
 
 st.header("My First App")
 uploaded_file = st.file_uploader("Choose a file")
+
+df1=pd.read_excel(uploaded_file)
